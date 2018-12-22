f181f8175ac201605f72f18ed757256fb70be233
Jeffrey Mercedes <Jeff.Mercedes@gmail.com>
2018-12-22 12:13:30 -0500

    .

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
   $(".profile-name").sticky({topSpacing:50});
   $(".profile-position").sticky({topSpacing:80});
+  $(window).bind('scroll', function() {
+  var navHeight = 145;
+    if ($(window).scrollTop() > navHeight) {
+      $(".profile-name").addClass("fixed-changed").removeClass("profile-name");
+    } else {
+      $(".fixed-changed").removeClass("fixed-changed").addClass("profile-name");
+    }
+  });
 });
