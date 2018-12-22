c673d3be00591b8559a459fcdb7fe4c84135b144
Jeffrey Mercedes <Jeff.Mercedes@gmail.com>
2018-12-22 14:15:49 -0500

    .

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  $(".info-name").sticky({topSpacing:57});
-  $(".info-position").sticky({topSpacing:77});
+  $(".info-name").sticky({topSpacing:56});
+  $(".info-position").sticky({topSpacing:76});
   $(window).bind('scroll', function() {
     if ($(window).scrollTop() > 145) {
       $(".profile-name").addClass("profile-name-shrink").removeClass("profile-name");
